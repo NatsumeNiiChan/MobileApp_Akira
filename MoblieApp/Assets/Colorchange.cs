79dd5dbac2376d42f6833d5e15ac0e54d9100625
Natsume <natsumeniichan@gmail.com>
2023-11-23 14:33:25 +0100

    Colorchange

--- MoblieApp/Assets/Colorchange.cs
+++ MoblieApp/Assets/Colorchange.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Colorchange : MonoBehaviour
 {
+    
+
     private void OnMouseDown()
     {
-        
+        GetComponent<Renderer>().material.color = Color.red;
     }
 }
